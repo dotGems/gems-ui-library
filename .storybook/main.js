0577d744a066743754169682033d5b9bdc7f220e
Denis Carriere <carriere.denis@gmail.com>
2021-12-05 14:21:20 -0500

    update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,6 @@
 module.exports = {
   stories: [
-    '../stories/welcome.stories.@(ts|tsx|js|jsx)',
-    '../stories/1_core/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/2_blend/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/3_collections/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/4_drops/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/5_secondary_market/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/6_chain/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/7_dotgems/**/*.stories.@(ts|tsx|js|jsx)',
-    '../stories/9_utils/**/*.stories.@(ts|tsx|js|jsx)',
+    '../stories/**/*.@(ts|tsx|js|jsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
