30938e3a2dd999fe4334e0348c8bea598be9bff9
Mellowjay <jbhavsar007@gmail.com>
2022-02-07 16:31:28 -0500

    update image source to cloud functions api

--- src/data/constants/urls.ts
+++ src/data/constants/urls.ts
@@ -1,2 +1,4 @@
-export const IPFS_SOURCE = "https://ipfs.atomichub.io/ipfs/";
+export const IPFS_SOURCE = "https://functions.eosn.io/v1/ipfs/";
+export const IPFS_VIDEO_SOURCE = "https://ipfs.atomichub.io/ipfs/";
+export const IPFS_SOURCE_FALLBACK = "https://cloudflare-ipfs.com/ipfs/";
 export const COLLECTION_SOURCE = "https://eos.atomichub.io/explorer/collection/";
